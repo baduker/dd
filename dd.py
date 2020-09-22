076ccd23608916d619d39ebebbabbebdcf7fdbae
baduker <baduker@protonmail.ch>
2020-09-22 20:28:03 +0200

    Version 0.0.1b

--- dd.py
+++ dd.py
@@ -1,30 +1,34 @@
 import datetime
+import logging
 import os
+
+from typing import List
 
 import requests
 from lxml import html
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 
-today = datetime.datetime.today().date()
-comic_src = '//*[@class="img-responsive img-comic"]/@src'
-comic_title = '//*[@class="img-responsive img-comic"]/@alt'
-
-
 def head_option(_iter: list) -> str:
-    return next(iter(_iter), None)
+    return next(iter(_iter), "")
 
 
-def get_dilbert() -> list:
-    response = requests.get(f"https://dilbert.com/strip/{today}").text
-    tree = html.fromstring(response)
-    src = head_option(tree.xpath(comic_src))
-    title = head_option(tree.xpath(comic_title))
+def get_page():
+    today = datetime.datetime.today().date()
+    return requests.get(f"https://dilbert.com/strip/{today}").text
+
+
+def get_dilbert() -> List[str]:
+    _xpath_root = '//*[@class="img-responsive img-comic"]'
+    tree = html.fromstring(get_page())
+    src = head_option(tree.xpath(f"{_xpath_root}/@src"))
+    title = head_option(tree.xpath(f"{_xpath_root}/@alt"))
     return [src, title]
 
 
 def send_dilbert(event, context):
     img_src, title = get_dilbert()
+    print(title, img_src)
     webhook = DiscordWebhook(url=os.getenv("WEBHOOK"))
     embed = DiscordEmbed(
         title=title,
